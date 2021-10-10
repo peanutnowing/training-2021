32331f0a84c5f511e467b94f8d1f7ab6662adfc5
peanutplan <peanutplan@qq.com>
2021-10-10 22:04:42 +0800

    第9周

--- homework/himly-tcc-dubbo/transaction/src/main/java/com/transaction/demo/service/impl/TransactionServiceImpl.java
+++ homework/himly-tcc-dubbo/transaction/src/main/java/com/transaction/demo/service/impl/TransactionServiceImpl.java
@@ -1,19 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+
 
 package com.transaction.demo.service.impl;
 
@@ -35,7 +20,7 @@ public class TransactionServiceImpl implements TransactionService {
     final private AccountService accountService;
 
     /**
-     * 这个注入很关键，这样注入就能进入RPC的切面，没有就报错
+     *
      * @param accountService
      */
     @Autowired(required = false)
@@ -51,7 +36,7 @@ public class TransactionServiceImpl implements TransactionService {
     }
 
     private void transactionA() {
-        log.info("============py one dubbo try 执行确认付款接口===============");
+        log.info("============pay one dubbo try 执行确认付款接口===============");
         Account account = new Account();
         account.setId(1L);
         account.setUs_wallet(-1L);
@@ -60,7 +45,7 @@ public class TransactionServiceImpl implements TransactionService {
     }
 
     private void transactionB() {
-        log.info("============py two dubbo try 执行确认付款接口===============");
+        log.info("============pay two dubbo try 执行确认付款接口===============");
         Account account = new Account();
         account.setId(2L);
         account.setUs_wallet(1L);
